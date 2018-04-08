b4ef11b94dccc49f33279611b3d6893d1cf81de0
buckfast <owwbua@gmail.com>
2018-04-08 21:31:30 +0300

    rest

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+let express = require('express');
+let router = express.Router();
+const indexController = require("../controllers/indexController");
+/* GET home page. */
+router.get('/', indexController.index_get);
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let router = express.Router();
+const usersController = require("../controllers/usersController");
+/* GET home page. */
+router.get('/', usersController.users_get);
+router.post('/', usersController.users_post);
+router.get('/:id/', usersController.user_id_get);
+router.post('/:id/', usersController.user_id_post);
+router.delete('/:id/', usersController.user_id_delete);
+
+module.exports = router;
